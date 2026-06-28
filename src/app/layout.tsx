c568620e1ac36648ef551abd2a8a684d93839708
Sumit Kumar <sumit@Sumits-MacBook-Air.local>
2026-06-28 20:49:11 +0530

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,14 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
+          enableSystem={false}
           enableColorScheme
         >
           <Navbar />
