63475f34292c77f3f8dfc044b9c23706a663744e
Sumit Kumar <sumit@Sumits-MacBook-Air.local>
2026-06-28 20:07:03 +0530

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // Forces Next.js to build a static production export
-  images: {
-    unoptimized: true, // Required for static exports if using next/image
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
